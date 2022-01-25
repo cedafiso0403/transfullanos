874ad0de8ba8e71d2c08ef8e00a4d4976371d175
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-01-25 00:12:26 -0800

    Homehero sliders added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/Base.scss';
+import './styles/style.scss';
 import App from './App';
 
 ReactDOM.render(
--- src/pages/ContentPages/home.jsx
+++ src/pages/ContentPages/home.jsx
@@ -1,13 +1,28 @@
 import Header from "../../components/ContentPages/header";
-import React from 'react';
+import React from 'react'
+import slide1 from "../../images/slide-1.jpg"
+import slide2 from "../../images/slide-2.jpg"
+import slide3 from "../../images/slide-3.jpg"
 
 const Home = () =>{
     return(
         <div>
             <Header></Header>
-            <div>
-                <h2>Hola</h2>
-            </div>
+            <main>
+                <div className="slider-frame">
+                    <div className="slide-images">
+                        <div className="img-container">
+                            <img src={slide1} alt="Slide 1"/>
+                        </div>
+                        <div className="img-container">
+                            <img src={slide2} alt="Slide 2"></img>
+                        </div>
+                        <div className="img-container">
+                            <img src={slide3} alt="Slide 3"></img>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
