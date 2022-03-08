3d0ff61adde829923ad5fde943e2c7db6d8983d0
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-03-08 11:25:06 -0800

    Styles and formating for home

--- src/components/ContentPages/header.jsx
+++ src/components/ContentPages/header.jsx
@@ -5,14 +5,14 @@ import React from 'react';
 const Header = () => {
     return (
         <div id="header_box">
-            <h1>Transfullanos</h1>
+            <h1>TRANSFULLANOS</h1>
             <nav id="header_navbox">
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/products">Products</Link></li>
                     <li><Link to="/about">About us</Link></li>
                     <li><Link to="/contact">Contact us</Link></li>
-                    <li><Link to="/login">Log in</Link></li>
+                    <li className="login-button"><Link className="text-color" to="/login">Log in</Link></li>
                 </ul>
             </nav>
         </div>
--- src/pages/ContentPages/home.jsx
+++ src/pages/ContentPages/home.jsx
@@ -5,15 +5,15 @@ import slide1 from "../../images/slide-1.jpg"
 import slide2 from "../../images/slide-2.jpg"
 import slide3 from "../../images/slide-3.jpg"
 
-const Home = () =>{
-    return(
+const Home = () => {
+    return (
         <div>
             <Header></Header>
             <main>
                 <div className="slider-frame">
                     <div className="slide-images">
                         <div className="img-container">
-                            <img src={slide1} alt="Slide 1"/>
+                            <img src={slide1} alt="Slide 1" />
                         </div>
                         <div className="img-container">
                             <img src={slide2} alt="Slide 2"></img>
@@ -22,9 +22,17 @@ const Home = () =>{
                             <img src={slide3} alt="Slide 3"></img>
                         </div>
                     </div>
+                    <div className="background-home"></div>
+                    <div className="text-container">
+                        <h3>Lorem ipsum</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
                 </div>
             </main>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
