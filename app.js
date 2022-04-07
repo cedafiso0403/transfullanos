f51941aebb9172d95a0a288be9de7c85b38dd0b7
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-04-07 03:12:00 -0700

    Aqui

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-
 const connection = require("./db/connection.js");
 const router = require("./routes/index.js");
-const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.use(express.static('public'));
@@ -14,7 +13,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req,res,next)=>{
-
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "auth-token, Origin, Content-Type, Accept");
